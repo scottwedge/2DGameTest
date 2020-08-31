347651d2b232de1d78a8dd144c89e832a331a311
Christian Loizou <loizouchristian@gmail.com>
2020-08-31 02:26:24 +0100

    Some minor additions to "main.py". The last update for the night

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ from cocos import layer, scene
 from cocos.director import director
 from cocos.text import Label
 
+# GLOBALS
+WINDOW_CAPTION = 'A promising-looking Cocos2D application - by Christian Loizou'
+TITLE_TEXT = 'Main Menu'
+TITLE_FONT_NAME = 'Times New Roman'
+TITLE_FONT_SIZE = 32
+
 
 class Application(layer.Layer):
-    def __init__(self, width, height, /):
+    def __init__(self):
         super().__init__()
-        self.title_label = Label('Main Menu', font_name='Times New Roman', font_size=32, anchor_x='center', anchor_y='center')
-        self.title_label.position = (width/2, height/2)
+        self.size = director.get_window_size()
+        self.title_label = Label(TITLE_TEXT, font_name=TITLE_FONT_NAME, font_size=TITLE_FONT_SIZE,
+            anchor_x='center', anchor_y='center')
+        self.title_label.position = tuple(map(lambda p: p/2, self.size))
         self.add(self.title_label)
 
 if __name__ == '__main__':
-    director.init(width=1280, height=720, caption='Hello, World!')
-    application = Application(1280, 720,)
+    director.init(width=1280, height=720, caption=WINDOW_CAPTION)
+    application = Application()
     scene = scene.Scene(application)
     director.run(scene)
